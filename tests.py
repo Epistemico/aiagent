9418f1262fb604a6f182f55340757e8d74691b53
Epistemico <Epistemical@outlook.com>
2025-07-26 01:08:36 +0200

    Added function with tests that creates or writes existing file within a given directory

--- tests.py
+++ tests.py
@@ -1,48 +1,64 @@
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+# from functions.get_files_info import get_files_info
+# from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
-def testGetFilesInfo():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
+# def testGetFilesInfo():
+#     result = get_files_info("calculator", ".")
+#     print("Result for current directory:")
+#     print(result)
+#     print()
+
+#     result = get_files_info("calculator", "pkg")
+#     print("Result for 'pkg' directory:")
+#     print(result)
+#     print()
+
+#     result = get_files_info("calculator", "/bin")
+#     print("Result for '/bin/' directory:")
+#     print(result)
+#     print()
+
+#     result = get_files_info("calculator", "../")
+#     print("Result for '../' directory:")
+#     print(result)
+#     print()
+
+
+# def testGetFileContent():
+#     result = get_file_content("calculator", "lorem.txt")
+#     print(result)
+
+#     result = get_file_content("calculator", "main.py")
+#     print(result)
+
+#     result = get_file_content("calculator", "pkg/calculator.py")
+#     print(result)
+
+#     result = get_file_content("calculator", "/bin/cat")
+#     print(result)
+#     print()
+
+#     result = get_file_content("calculator", "pkg/does_not_exist.py")
+#     print(result)
+#     print()
+
+
+def testWriteFile():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
     print()
 
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
     print()
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin/' directory:")
-    print(result)
-    print()
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
-    print()
-
-
-def testGetFileContent():
-    result = get_file_content("calculator", "lorem.txt")
-    print(result)
-
-    result = get_file_content("calculator", "main.py")
-    print(result)
-
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print(result)
-
-    result = get_file_content("calculator", "/bin/cat")
-    print(result)
-    print()
-
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
     print()
 
 
 if __name__ == "__main__":
-    testGetFilesInfo()
-    testGetFileContent()
+    # testGetFilesInfo()
+    # testGetFileContent()
+    testWriteFile()
