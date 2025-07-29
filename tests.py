37de5fd33cb058a0d9e56d57974babf8bec9d2a2
Epistemico <Epistemical@outlook.com>
2025-07-29 18:20:01 +0200

    Added function with tests to execute python files

--- tests.py
+++ tests.py
@@ -1,47 +1,48 @@
-# from functions.get_files_info import get_files_info
-# from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
-# def testGetFilesInfo():
-#     result = get_files_info("calculator", ".")
-#     print("Result for current directory:")
-#     print(result)
-#     print()
+def testGetFilesInfo():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print()
 
-#     result = get_files_info("calculator", "pkg")
-#     print("Result for 'pkg' directory:")
-#     print(result)
-#     print()
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+    print()
 
-#     result = get_files_info("calculator", "/bin")
-#     print("Result for '/bin/' directory:")
-#     print(result)
-#     print()
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin/' directory:")
+    print(result)
+    print()
 
-#     result = get_files_info("calculator", "../")
-#     print("Result for '../' directory:")
-#     print(result)
-#     print()
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+    print()
 
 
-# def testGetFileContent():
-#     result = get_file_content("calculator", "lorem.txt")
-#     print(result)
+def testGetFileContent():
+    result = get_file_content("calculator", "lorem.txt")
+    print(result)
 
-#     result = get_file_content("calculator", "main.py")
-#     print(result)
+    result = get_file_content("calculator", "main.py")
+    print(result)
 
-#     result = get_file_content("calculator", "pkg/calculator.py")
-#     print(result)
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
 
-#     result = get_file_content("calculator", "/bin/cat")
-#     print(result)
-#     print()
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
+    print()
 
-#     result = get_file_content("calculator", "pkg/does_not_exist.py")
-#     print(result)
-#     print()
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print(result)
+    print()
 
 
 def testWriteFile():
@@ -58,7 +59,27 @@ def testWriteFile():
     print()
 
 
+def testRunPythonFile():
+    result = run_python_file("calculator", "main.py")
+    print(result)
+
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+    print()
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+    print()
+
+
 if __name__ == "__main__":
-    # testGetFilesInfo()
-    # testGetFileContent()
+    testGetFilesInfo()
+    testGetFileContent()
     testWriteFile()
+    testRunPythonFile()
